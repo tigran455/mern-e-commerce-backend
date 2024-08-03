4e41aa2f4dc4039cad68fe76611b9c900d44bbfd
Tigran <tigran19910315@gmail.com>
2024-08-03 19:07:00 +0400

    creating first api

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const connectDB = require('./config/db')
+const router = require('./routes')
 
 
 const app = express()
-app.use(cors)
+app.use(cors())
+app.use(express.json())
+
+app.use("/api",router)
 
 const PORT = 8080 || process.env.PORT
 
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const router = express.Router()
+
+const userSignUpController = require("../controller/userSignUp")
+
+router.post("/signup",userSignUpController)
+
+module.exports = router
