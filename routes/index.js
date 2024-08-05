77b484823a9a9cbec0d70c14b593840b2d3ce406
Tigran <tigran19910315@gmail.com>
2024-08-05 22:40:38 +0400

    new folder

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,16 @@ const express = require('express')
 
 const router = express.Router()
 
-const userSignUpController = require("../controller/userSignUp")
-const userSignInController = require("../controller/userSignIn")
-const userDetailsController = require("../controller/userDetails");
+const userSignUpController = require("../controller/user/userSignUp")
+const userSignInController = require("../controller/user/userSignIn")
+const userDetailsController = require("../controller/user/userDetails");
 const authToken = require("../middleware/authToken");
-const userLogout = require("../controller/userLogout");
-const allUsers = require("../controller/allUsers");
-const updateUser = require("../controller/updateUser");
-const UploadProductController = require("../controller/uploadProduct");
-const getProductController = require("../controller/getProduct");
-const updateProductController = require("../controller/updateProduct");
+const userLogout = require("../controller/user/userLogout");
+const allUsers = require("../controller/user/allUsers");
+const updateUser = require("../controller/user/updateUser");
+const UploadProductController = require("../controller/user/uploadProduct");
+const getProductController = require("../controller/user/getProduct");
+const updateProductController = require("../controller/user/updateProduct");
 
 router.post("/signup",userSignUpController)
 router.post("/signin",userSignInController)
