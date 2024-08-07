9ed4a3a328d503cde75260d9798fb4dd0db0e327
Tigran <tigran19910315@gmail.com>
2024-08-07 20:36:36 +0400

    add to cart

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,8 @@ const updateProductController = require("../controller/product/updateProduct");
 const getCategoryProduct = require("../controller/product/getCategoryProductOne");
 const getCategoryWiseProduct = require("../controller/product/getCategoryWiseProduct");
 const getProductDetails = require("../controller/product/getProductDetails");
+const addToCartController = require("../controller/user/addToCartController");
+const countAddToCartProduct = require("../controller/user/countAddToCartProduct");
 
 router.post("/signup",userSignUpController)
 router.post("/signin",userSignInController)
@@ -32,6 +34,10 @@ router.post("/update-product",authToken,updateProductController)
 router.get("/get-categoryProduct",getCategoryProduct)
 router.post("/category-product",getCategoryWiseProduct)
 router.post("/product-details",getProductDetails)
+
+//user add to cart
+router.post("/addtocart",authToken,addToCartController)
+router.get("/countAddToCartProduct",authToken,countAddToCartProduct)
 
 
 module.exports = router
